d72f260b5feea5068e7865cc4dc09f3d30f22c2d
marky <markism1224amarille@gmail.com>
2026-07-09 17:04:32 +0800

    feat: add di for the ef persistence

--- src/Marky.Framework.Persistence.EntityFramework/DependencyInjection.cs
+++ src/Marky.Framework.Persistence.EntityFramework/DependencyInjection.cs
@@ -1,6 +1,26 @@
+using Marky.Framework.Domain;
+using Marky.Framework.Persistence.Abstraction;
+using Marky.Framework.Persistence.EntityFramework.Interceptor;
+using Marky.Framework.Persistence.EntityFramework.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Marky.Framework.Persistence.EntityFramework;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddFrameworkPersistenceEngine<TResolver>(
+        this IServiceCollection services
+    )
+        where TResolver : class, IEntityContextResolver
+    {
+        services.AddScoped<PersistenceAuditInterceptor>();
+        services.AddScoped<OutboxRedirectInterceptor>();
+
+        services.AddSingleton<IEntityContextResolver, TResolver>();
+
+        services.AddScoped(typeof(Repository<,>));
+        services.AddScoped(typeof(IRepository<>), typeof(DynamicRepositoryProxy<>));
+
+        return services;
+    }
 }
